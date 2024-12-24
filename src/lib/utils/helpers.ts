ce005caf9ed8e925468302f5584c37e9356f065f
paw1a <paw1a@yandex.ru>
2024-12-24 21:38:48 +0300

    fix

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -20,9 +20,6 @@ export const isValidPasswordMedium = (password) => {
 
 export const setAuthToken = ({cookies, token}) => {
 	cookies.set('AuthorizationToken', `Bearer ${token}`, {
-		httpOnly: true,
-		secure: true,
-		sameSite: 'strict',
 		maxAge: 60 * 60 * 24,
 		path: '/'
 	});
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit(), purgeCss()],
 	server: {
+		host: '0.0.0.0',
 		proxy: {
 			"/api": {
 				target: "http://185.25.51.102:8080/api",
